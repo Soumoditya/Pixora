e9a53a9bcb77b4cb2c026db0e07628699eb947ca
Soumoditya <soumodityapramanik@gmail.com>
2025-10-15 19:35:07 +0530

    Create app.jsx

---
+++ src/app.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import AuthPage from "./pages/AuthPage";
+import MainPage from "./pages/MainPage";
+import ProfilePage from "./pages/ProfilePage";
+
+function App() {}
+App.ViewAuth = AuthPage;
+App.Main = MainPage;
+App.Profile = ProfilePage;
+
+export default App;
